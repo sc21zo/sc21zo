b19f68abac9db25e9821deb3cb28b6e41be744d5
sc21zo <sc21zo@leeds.ac.uk>
2022-08-13 02:58:19 +0800

    adding files

---
+++ SR/train.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+import os.path as osp
+
+from basicsr.train import train_pipeline
+
+if __name__ == '__main__':
+    root_path = osp.abspath(osp.join(__file__, osp.pardir))
+    train_pipeline(root_path)
